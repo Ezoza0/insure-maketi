d855cdf290131059662f09095eb2ebf13e6e7188
Ezoza0 <abdukarimovaezoza54@gmail.com>
2023-08-04 09:56:55 +0500

    Add responsive

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var elHeaderList = document.querySelector(".header-list");
+var elBurger = document.querySelector(".burger");
+
+
+elBurger.addEventListener("click", function() {
+    elHeaderList.classList.toggle("header-list-menu")
+})
